445977619b0eba2221ec49ba476d887e1c2b144a
RegishShrestha <regishshrestha@gmail.com>
2022-09-07 09:31:16 +0545

    land price

--- landpricescraping/main.py
+++ landpricescraping/main.py
@@ -1,53 +1,69 @@
-# from bs4 import BeautifulSoup
-# from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# import time
-
-# CHROME_DRIVER_PATH="/home/regish/Desktop/development/chromedriver"
-# driver=webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
-# driver.get('https://forms.gle/DVYDGW4U5NamL4L68')
-
-# EMAIL="shresthapython@gmail.com"
-# PASSWORD="abcd1234()"
-
-# time.sleep(5)
-# signin=driver.find_element('xpath',"/html/body/div[2]/div/div[2]/div[3]/div[2]/span/span")
-# signin.click()
-
-
-# time.sleep(5)
-# gmail=driver.find_element("id","identifierId")
-# gmail.send_keys(EMAIL)
-# gmail.send_keys(Keys.ENTER)
-
-# time.sleep(6)
-# try_again=driver.find_element('xpath','//*[@id="next"]/div/div/a')
-# try_again.click()
-
-# time.sleep(20)
-# gmail_again=driver.find_element('xpath','//*[@id="identifierId"]')
-# gmail_again.send_keys(EMAIL)
-# gmail_again.send_keys(Keys.ENTER)
-
-# time.sleep(20)
-# # time.sleep(3)
-# # passoword=driver.find_element("name","Passwd")
-# # passoword.send_keys(PASSWORD)
-
-# # address=driver.find_element('class name','whsOnd')
-# # # address.send_keys('hey')
-
-# # soup=BeautifulSoup('https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.56276167822266%2C%22east%22%3A-122.30389632177734%2C%22south%22%3A37.69261345230467%2C%22north%22%3A37.857877098316834%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D')
-
-# # //*[@id="identifierId"]
-
 from bs4 import BeautifulSoup
 import requests
-import lxml
+from selenium import webdriver
+import time
+
+header = {
+    "User-Agent": "/home/regish/Desktop/development/chromedriver",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(
+    "https://www.zillow.com/homes/San-Francisco,-CA_rb/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22usersSearchTerm%22%3A%22San%20Francisco%2C%20CA%22%2C%22mapBounds%22%3A%7B%22west%22%3A-122.55177535009766%2C%22east%22%3A-122.31488264990234%2C%22south%22%3A37.69926912019228%2C%22north%22%3A37.851235694487485%7D%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A20330%2C%22regionType%22%3A6%7D%5D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D",
+    headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
 
 
-response=requests.get("https://www.zillow.com/homes/for_rent/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-123.04032362890625%2C%22east%22%3A-121.82633437109375%2C%22south%22%3A37.493074369466974%2C%22north%22%3A38.0564353294575%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%5D%7D")
+all_link_elements = soup.select(".list-card-top a")
 
-zillow=response.text
-soup=BeautifulSoup(zillow,'html.parser')
-print(soup.find(class_="list-card-price"))
+all_links = []
+for link in all_link_elements:
+    href = link["href"]
+    print(href)
+    if "http" not in href:
+        all_links.append(f"https://www.zillow.com{href}")
+    else:
+        all_links.append(href)
+
+all_address_elements = soup.select(".list-card-info address")
+all_addresses = [address.get_text().split(" | ")[-1] for address in all_address_elements]
+
+all_price_elements = soup.select(".list-card-heading")
+all_prices = []
+for element in all_price_elements:
+    # Get the prices. Single and multiple listings have different tag & class structures
+    try:
+        # Price with only one listing
+        price = element.select(".list-card-price")[0].contents[0]
+    except IndexError:
+        print('Multiple listings for the card')
+        # Price with multiple listings
+        price = element.select(".list-card-details li")[0].contents[0]
+    finally:
+        all_prices.append(price)
+
+
+# Create Spreadsheet using Google Form
+# Substitute your own path here 👇
+chrome_driver_path = "/home/regish/Desktop/development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+# for n in range(len(all_links)):
+#     # Substitute your own Google Form URL here 👇
+#     driver.get(https://forms.gle/eRdM9QQS6Hpgsdmv9)
+
+#     time.sleep(2)
+#     address = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     price = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     link = driver.find_element_by_xpath(
+#         '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+#     submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+
+#     address.send_keys(all_addresses[n])
+#     price.send_keys(all_prices[n])
+#     link.send_keys(all_links[n])
+#     submit_button.click()
