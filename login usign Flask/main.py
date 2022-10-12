07d822a8fb4c7f29385d67e82eb3044f14f9d513
RegishShrestha <regishshrestha@gmail.com>
2022-10-12 15:12:19 +0545

    login updated flask bootstrap added

--- login usign Flask/main.py
+++ login usign Flask/main.py
@@ -2,18 +2,19 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,Email,Length
 
 from flask_bootstrap import Bootstrap
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email',validators=[DataRequired()])
-    password = PasswordField(label='Password',validators=[DataRequired()])
+    email = StringField(label='Email',validators=[Email(message=("u'That\'s not a valid email address."))])
+    password = PasswordField(label='Password',validators=[DataRequired(u'Little short for an email address?'),Length(min=8)])
     submit=SubmitField(label="Log In")
 
 
 
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 @app.route("/")
